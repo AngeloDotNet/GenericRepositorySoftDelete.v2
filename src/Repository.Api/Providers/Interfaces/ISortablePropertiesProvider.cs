4ce21f48e119d04a69082db042f4e45970fb3ede
Angelo Pirola <angelo9871@gmail.com>
2026-03-10 08:52:29 +0100

    Add ISortablePropertiesProvider interface for sortable fields
    Introduced ISortablePropertiesProvider in Repository.Api.Providers.Interfaces. This interface provides methods to retrieve sortable property names for a given entity type, supporting both generic and non-generic usage.

---
+++ src/Repository.Api/Providers/Interfaces/ISortablePropertiesProvider.cs
@@ -0,0 +1,7 @@
+﻿namespace Repository.Api.Providers.Interfaces;
+
+public interface ISortablePropertiesProvider
+{
+    IEnumerable<string> GetSortableProperties(Type entityType);
+    IEnumerable<string> GetSortableProperties<T>();
+}

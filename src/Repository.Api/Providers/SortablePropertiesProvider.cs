fb10cc87e86075523c6b45109abbe310d140a3e7
Angelo Pirola <angelo9871@gmail.com>
2026-03-10 08:52:39 +0100

    Add SortablePropertiesProvider for sortable property lookup
    Introduced the SortablePropertiesProvider class in the Repository.Api.Providers namespace. This class implements ISortablePropertiesProvider and allows retrieval of sortable property names for entity types using a provided type-to-property map. Includes null checks and methods for both type and generic access.

---
+++ src/Repository.Api/Providers/SortablePropertiesProvider.cs
@@ -0,0 +1,25 @@
+﻿using Repository.Api.Providers.Interfaces;
+
+namespace Repository.Api.Providers;
+
+public class SortablePropertiesProvider(IDictionary<Type, string[]> map) : ISortablePropertiesProvider
+{
+    private readonly IReadOnlyDictionary<Type, string[]> map = new Dictionary<Type, string[]>(map ?? throw new ArgumentNullException(nameof(map)));
+
+    public IEnumerable<string> GetSortableProperties(Type entityType)
+    {
+        if (entityType == null)
+        {
+            throw new ArgumentNullException(nameof(entityType));
+        }
+
+        if (map.TryGetValue(entityType, out var arr))
+        {
+            return arr;
+        }
+
+        return [];
+    }
+
+    public IEnumerable<string> GetSortableProperties<T>() => GetSortableProperties(typeof(T));
+}

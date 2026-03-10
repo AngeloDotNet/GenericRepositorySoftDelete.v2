c36211b02b2c08ccbb04b2bdbfeb575737410aca
Angelo Pirola <angelo9871@gmail.com>
2026-03-10 08:52:05 +0100

    Remove soft-delete comment from Product class
    Removed the // soft-delete props comment from the Product class in Product.cs. No changes were made to the functional code.

--- src/Repository.Api/Entities/Product.cs
+++ src/Repository.Api/Entities/Product.cs
@@ -7,7 +7,6 @@ public class Product : BaseEntity, ISoftDelete
     public string Name { get; set; } = null!;
     public decimal Price { get; set; }
 
-    // soft-delete props
     public bool IsDeleted { get; set; }
     public DateTime? DeletedAt { get; set; }
 }

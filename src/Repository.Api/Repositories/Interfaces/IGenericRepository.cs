99c01945cca62d70d84ae934496794cccb7bc9a1
Angelo Pirola <angelo9871@gmail.com>
2026-03-11 08:52:47 +0100

    Add paged query method to IGenericRepository interface
    Added GetPagedAsync to IGenericRepository<T> to enable paginated queries with optional filtering. Also removed a redundant comment from the Query() method signature.

--- src/Repository.Api/Repositories/Interfaces/IGenericRepository.cs
+++ src/Repository.Api/Repositories/Interfaces/IGenericRepository.cs
@@ -7,10 +7,12 @@ public interface IGenericRepository<T> where T : BaseEntity
 {
     Task<T?> GetByIdAsync(int id);
     Task<IEnumerable<T>> GetAllAsync();
-    IQueryable<T> Query(); // per query avanzate
+    IQueryable<T> Query();
     Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
     Task AddAsync(T entity);
     void Update(T entity);
     Task DeleteAsync(T entity); // soft delete
     Task HardDeleteAsync(T entity); // hard delete
+
+    Task<PagedResult<T>> GetPagedAsync(QueryParameters parameters, Expression<Func<T, bool>>? filter = null);
 }

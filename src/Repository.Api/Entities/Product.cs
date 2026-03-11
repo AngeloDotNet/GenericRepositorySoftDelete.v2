ca320c0f23fce63bb6c6dee6af0e909bcc71e906
Angelo Pirola <angelo9871@gmail.com>
2026-03-11 08:52:40 +0100

    Enable sorting on Product Name and Price properties
    Added the [Sortable] attribute to the Name and Price fields in the Product class, allowing these properties to be used in sorting operations. Also included the necessary using directive for Repository.Api.Attributes.

--- src/Repository.Api/Entities/Product.cs
+++ src/Repository.Api/Entities/Product.cs
@@ -1,10 +1,14 @@
-﻿using Repository.Api.Entities.Interfaces;
+﻿using Repository.Api.Attributes;
+using Repository.Api.Entities.Interfaces;
 
 namespace Repository.Api.Entities;
 
 public class Product : BaseEntity, ISoftDelete
 {
+    [Sortable] // abilitata per il sorting
     public string Name { get; set; } = null!;
+
+    [Sortable] // abilitata per il sorting
     public decimal Price { get; set; }
 
     public bool IsDeleted { get; set; }

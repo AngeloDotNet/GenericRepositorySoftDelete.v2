29833e9b3f4080bea5929492153377c4770ebad4
Angelo Pirola <angelo9871@gmail.com>
2026-03-11 08:52:33 +0100

    Add AutoMapper profile for Product and DTO mappings
    Created MappingProfile.cs to configure AutoMapper mappings between Product entities and their corresponding DTOs (ProductDto, ProductCreateDto, ProductUpdateDto), enabling streamlined object conversions throughout the application.

---
+++ src/Repository.Api/Mapping/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Repository.Api.DTOs;
+using Repository.Api.Entities;
+
+namespace Repository.Api.Mapping;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Product, ProductDto>().ReverseMap();
+        CreateMap<ProductCreateDto, Product>();
+        CreateMap<ProductUpdateDto, Product>();
+    }
+}
